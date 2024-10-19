a301aa950c8d347d3460ae408dd9830ab03788d9
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-10-19 02:01:53 +0200

    initial commit

---
+++ ngxFormly-basics/ngxFormlyInputs/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+
+// Multi field sortable table, call to mock api, see above
+  products3: any[] = [];
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.httpClient.get<any[]>('products3.json').subscribe(data => {
+      this.products3 = data;
+    });
+  }
+}
